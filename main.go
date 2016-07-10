a96184f2ddd88b7a82f9de3cc9e7062e1ce7c982
Danielle Tomlinson <dan@tomlinson.io>
2016-07-10 18:59:08 -0400

    Cleanup

--- main.go
+++ main.go
@@ -14,30 +14,33 @@ var fromHost = flag.String("from", "localhost:80", "The proxy server's host.")
 var toHost = flag.String("to", "localhost:8000", "The host that the proxy " +
              " server should forward requests to.")
 
-func NewNotificationProxy(to *url.URL) *httputil.ReverseProxy {
-  director := func(req *http.Request) {
+func GetValueOrFallbackFromValues(v url.Values, key string, fallback string) string {
+  value := v.Get(key)
+  if value == "" {
+    value = fallback
+  }
+
+  return value
+}
+
+func RewriteRequest(req *http.Request, to *url.URL) {
     query := req.URL.Query()
-    messages := query["message"]
-    services := query["service"]
-
-    var service = "GENERIC"
-    if len(services) > 0 {
-      service = services[0]
-    }
-
-    var message = fmt.Sprintf("[%s]", service)
-    if len(messages) > 0 {
-      message = fmt.Sprintf("%s %s", message, messages[0])
-    }
-
-    bodyString := fmt.Sprintf("{\"text\":\"%s\"}", message)
-    body := ioutil.NopCloser(bytes.NewBufferString(bodyString))
+    message := GetValueOrFallbackFromValues(query, "message", "...")
+    service := GetValueOrFallbackFromValues(query, "service", "GENERIC")
+    text := fmt.Sprintf("[%s] %s", service, message)
+    json := fmt.Sprintf("{\"text\":\"%s\"}", text)
+    body := ioutil.NopCloser(bytes.NewBufferString(json))
 
     req.URL = to
     req.Host = to.Host
     req.Body = body
     req.Method = "POST"
     req.Header = http.Header{}
+}
+
+func NewNotificationProxy(to *url.URL) *httputil.ReverseProxy {
+  director := func(req *http.Request) {
+    RewriteRequest(req, to)
   }
 
   return &httputil.ReverseProxy{
