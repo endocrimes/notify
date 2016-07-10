55e657c0813b77702f8ac7d80a060c12273a93a1
Danielle Tomlinson <dan@tomlinson.io>
2016-07-10 19:15:29 -0400

    Refactorings

--- main.go
+++ main.go
@@ -14,6 +14,28 @@ var fromHost = flag.String("from", "localhost:80", "The proxy server's host.")
 var toHost = flag.String("to", "localhost:8000", "The host that the proxy " +
              " server should forward requests to.")
 
+func main() {
+  flag.Parse()
+  to, err := url.Parse(*toHost)
+  if err != nil {
+    fmt.Println(err)
+  }
+  handler := &RequestHandler{
+    Target: to,
+  }
+  proxy := NewNotificationProxy(handler)
+  fmt.Printf("Proxying %s->%s.\n", *fromHost, *toHost)
+  http.ListenAndServe(*fromHost, proxy)
+}
+
+// @!group Helpers
+
+func NewNotificationProxy(handler *RequestHandler) *httputil.ReverseProxy {
+  return &httputil.ReverseProxy{
+    Director: handler.handle,
+  }
+}
+
 func GetValueOrFallbackFromValues(v url.Values, key string, fallback string) string {
   value := v.Get(key)
   if value == "" {
@@ -23,7 +45,13 @@ func GetValueOrFallbackFromValues(v url.Values, key string, fallback string) str
   return value
 }
 
-func RewriteRequest(req *http.Request, to *url.URL) {
+// @!group Request Handler
+
+type RequestHandler struct {
+  Target *url.URL
+}
+
+func (r *RequestHandler)handle(req *http.Request) {
     query := req.URL.Query()
     message := GetValueOrFallbackFromValues(query, "message", "...")
     service := GetValueOrFallbackFromValues(query, "service", "GENERIC")
@@ -31,30 +59,9 @@ func RewriteRequest(req *http.Request, to *url.URL) {
     json := fmt.Sprintf("{\"text\":\"%s\"}", text)
     body := ioutil.NopCloser(bytes.NewBufferString(json))
 
-    req.URL = to
-    req.Host = to.Host
+    req.URL = r.Target
+    req.Host = r.Target.Host
     req.Body = body
     req.Method = "POST"
     req.Header = http.Header{}
-}
-
-func NewNotificationProxy(to *url.URL) *httputil.ReverseProxy {
-  director := func(req *http.Request) {
-    RewriteRequest(req, to)
-  }
-
-  return &httputil.ReverseProxy{
-    Director: director,
-  }
-}
-
-func main() {
-  flag.Parse()
-  to, err := url.Parse(*toHost)
-  if err != nil {
-    fmt.Println(err)
-  }
-  proxy := NewNotificationProxy(to)
-  fmt.Printf("Proxying %s->%s.\n", *fromHost, *toHost)
-  http.ListenAndServe(*fromHost, proxy)
 }
